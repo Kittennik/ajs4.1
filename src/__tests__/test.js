1c48a3e35a7a2861fdf84f5af5646fc7baf236a2
Kate Pronina <kateadm@narod.ru>
2020-03-07 17:52:20 +0300

    Выполненное задание

---
+++ src/__tests__/test.js
@@ -0,0 +1,22 @@
+import typeChecking from '../js/app';
+
+test('Проверка на число', () => {
+  const testQuantity = 5;
+  const received = typeChecking(testQuantity);
+  const expected = 5;
+  expect(received).toBe(expected);
+});
+
+test('Проверка на строку', () => {
+  const testQuantity = 'пять';
+  const received = typeChecking(testQuantity);
+  const expected = new Error('Полученные данные не являются числом в десятичной форме');
+  expect(received).toEqual(expected);
+});
+
+test('Проверка на символы', () => {
+  const testQuantity = '-76р%';
+  const received = typeChecking(testQuantity);
+  const expected = new Error('Полученные данные не являются числом в десятичной форме');
+  expect(received).toEqual(expected);
+});
